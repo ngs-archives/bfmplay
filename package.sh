78c73d6923cf8f0dd510411060e6823e1a2744ef
Atsushi NAGASE <atsn.ngs@gmail.com>
2012-10-12 04:00:10 +0900

    added icon

--- package.sh
+++ package.sh
@@ -1,1 +1,1 @@
-adt -package -storetype pkcs12 -keystore cert.pfx bfmplayer.air src/application.xml -C src index.html
+adt -package -storetype pkcs12 -keystore cert.pfx bfmplayer.air src/application.xml -C src index.html assets/icon16.png assets/icon32.png assets/icon48.png assets/icon128.png
